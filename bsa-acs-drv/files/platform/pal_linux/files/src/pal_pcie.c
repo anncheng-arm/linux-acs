64466507d7e6405ac0b33ad0b8033aba74da9dbb
Sujana M <sujana.m@arm.com>
2023-12-18 11:47:39 +0530

    Updated the PAL API pal_pcie_mem_get_offset
    - Updated the parameters of the PAL API
      pal_pcie_mem_get_offset for bsa and sbsa
    
    Signed-off-by: Sujana M <sujana.m@arm.com>

--- bsa-acs-drv/files/platform/pal_linux/files/src/pal_pcie.c
+++ bsa-acs-drv/files/platform/pal_linux/files/src/pal_pcie.c
@@ -680,9 +680,12 @@ pal_pcie_check_device_list()
           This offset is platform-specific. It needs to
           be modified according to the requirement.
 
+  @param  bdf      - PCIe BUS/Device/Function
+  @param  mem_type - If the memory is Pre-fetchable or Non-prefetchable memory
+  @return memory offset
 **/
 uint32_t
-pal_pcie_mem_get_offset(uint32_t type)
+pal_pcie_mem_get_offset(uint32_t bdf, uint32_t mem_type)
 {
 
   return 0;
